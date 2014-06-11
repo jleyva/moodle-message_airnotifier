c2617a4a15a816ece842739adcf94fc783cf2afd
Juan Leyva <juanleyvadelgado@gmail.com>
2014-06-11 23:37:40 +0200

    Version number must be lower than 26 version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2014060100;           // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version    = 2014011500;           // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires   = 2013051400;             // Requires this Moodle version.
 $plugin->release    = '2.5.0';
 $plugin->component  = 'message_airnotifier';  // Full name of the plugin (used for diagnostics).
