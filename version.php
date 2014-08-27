753e94388bddfd9bfe8b46da5a154bcad5450aaf
Juan Leyva <juanleyvadelgado@gmail.com>
2014-08-27 10:37:40 +0200

    Bump version number (release new version)

--- version.php
+++ version.php
@@ -23,6 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2014012802;           // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version    = 2014012803;           // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires   = 2013111800;             // Requires this Moodle version.
+$plugin->release    = '2.6.1';
 $plugin->component  = 'message_airnotifier';  // Full name of the plugin (used for diagnostics).
+$plugin->maturity   = MATURITY_STABLE;
