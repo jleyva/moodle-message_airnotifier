cc4e3e2b199c181149db85e4e3f7850a5930366a
Juan Leyva <juanleyvadelgado@gmail.com>
2014-11-08 23:15:50 +0100

    MOBILE-721 Version compatible with Airnotifier API v2

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2014011501;           // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version    = 2014011502;           // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires   = 2013051400;             // Requires this Moodle version.
-$plugin->release    = '2.5.1';
+$plugin->release    = '2.5.2';
 $plugin->component  = 'message_airnotifier';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->dependencies = array(
